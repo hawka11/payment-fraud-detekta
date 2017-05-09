2668a070cff2698330f33b7733ce0acd7e14bcfc
Keith Hawkridge <Keith.Hawkridge@auspost.com.au>
2017-05-09 15:29:38 +1000

    update script

--- environment/neo4j.sh
+++ environment/neo4j.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ansible-playbook  --inventory-file=./provision/inv_default -u vagrant --private-key=~/.vagrant.d/insecure_private_key ./provision/site.yml --tags neo4j
+vagrant destroy -f
+vagrant up
+ansible-playbook  --inventory-file=./provision/inv_default -u vagrant --private-key=~/.vagrant.d/insecure_private_key ./provision/site.yml --tags docker,neo4j
